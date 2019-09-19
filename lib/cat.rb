2b09d8af984708d641df04a84feb3e056a4e3b75
Kenneth Spatta <kenyounot@gmail.com>
2019-09-19 04:42:33 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
-  # code goes here
+  attr_reader :name 
+  attr_accessor :owner, :mood
+  @@all = []
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @cats = []
+    @@all << self
+  end
+  
+  def cat 
+    
+  end
+  
+  def self.all 
+    @@all
+  end
 end
