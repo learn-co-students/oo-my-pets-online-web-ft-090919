896fbc9fa6bd61789f37709fbd4d8b1122bd45de
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-18 22:05:05 -0400

    Feat: Wrote cat and owner classes, passing all tests

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,24 @@
 class Cat
-  # code goes here
+
+  @@all = []
+
+  attr_writer :mood
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def mood
+    @mood ||= "nervous"
+  end
+
 end
