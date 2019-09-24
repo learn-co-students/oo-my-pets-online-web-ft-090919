4b97ef4897c5f08477896f2da9e2dab6b6a76766
root <root@DESKTOP-2V0R1D8.localdomain>
2019-09-24 17:12:35 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,36 +1,36 @@
 class Owner
-  
+
   attr_reader :name, :species
-  
+
   @@all = []
-  
+
   def initialize(name = nil)
     @name = name
     @species = 'human'
-    
+
     @@all << self
   end
-  
+
   def say_species
     "I am a " + @species + "."
   end
-   
+
   def self.all
     @@all
   end
-  
+
   def self.count
     @@all.count
   end
-  
+
   def self.reset_all
     @@all.clear
   end
-  
+
   def cats
     Cat.all.select{|cat| cat.owner.name == self.name}
   end
-  
+
   def dogs
     Dog.all.select{|dog| dog.owner.name == self.name}
   end
