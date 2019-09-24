cd6a275622d27c15bc997bde70af61e0eee5da03
root <root@DESKTOP-2V0R1D8.localdomain>
2019-09-24 18:11:24 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,4 +33,36 @@ class Owner
   def dogs
     Dog.all.select{|dog| dog.owner == self}
   end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each{|dog| dog.mood = 'happy'}
+  end
+
+  def feed_cats
+    self.cats.each{|cat| cat.mood = 'happy'}
+  end
+
+  def sell_pets
+    self.dogs.each do |dog|
+      dog.mood = 'nervous'
+      dog.owner = nil
+    end
+    self.cats.each do |cat|
+      cat.mood = 'nervous'
+      cat.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
 end
